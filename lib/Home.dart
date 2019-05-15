3b4edcef3c92e3d0b113465ee844e83ac0001474
Yazdani <yaz4dani@gmail.com>
2019-05-15 21:45:50 +0600

    second commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -56,9 +56,60 @@ class _HomeState extends State<Home> {
           ),//first container end..
 
           //second container start.
+          new Container(
+            height: 170.0,
+            margin: EdgeInsets.all(10.0),
+            child: new ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                allData("asset/a.jpg", "Loren", "Ipsum"),
+                new SizedBox(width: 7.0,),
+                allData("asset/b.jpg", "Loren", "Ipsum"),
+                new SizedBox(width: 7.0,),
+                allData("asset/a.jpg", "Loren", "Ipsum")
+              ],
+            ),
+          ),//end second container..
 
         ],
       ),
     );
   }
+
+  Widget allData(String image,String num,String title){
+    return Container(
+      height: 150.0,
+      width: 150.0,
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Color(0xFF212120)
+      ),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          
+          new Image.asset(image,
+          height: 70.0,
+            width: 70.0,
+            fit: BoxFit.cover,
+          ),
+          new SizedBox(height: 10.0,),
+          new Text(num,
+          style: TextStyle(
+            fontSize: 14.0,
+            color: Colors.white
+          ),
+          ),
+          new SizedBox(height: 5.0,),
+          new Text(title,
+              style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.white
+          ),
+          )
+        ],
+      ),
+    );
+  }
+
 }
